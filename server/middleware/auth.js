d9fc7438bcd311bb4f794e4224359c80059a5303
jimmy094 <jimmyalvarezjr@gmail.com>
2023-02-02 17:05:08 -0500

    changed err to error

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -25,6 +25,6 @@ export const verifyToken = async ( req, res, next) => {
         req.user = verified;
         next();
     } catch (error) {
-        res.status(500).json({ error: err.message })
+        res.status(500).json({ error: error.message })
     }
 }
